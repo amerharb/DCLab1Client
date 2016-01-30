87831b99573c731a918752464a014c730e5898ac
amerharb <amer.harb@gmail.com>
2016-01-30 16:03:56 +0100

    not stable yet

--- src/dclab1client/DCLab1Client.java
+++ src/dclab1client/DCLab1Client.java
@@ -10,7 +10,7 @@ public class DCLab1Client
     private static final int PORT = 8000;
     private static String SERVER = "127.0.0.1";
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         String server = SERVER;
         int port = PORT;
@@ -25,43 +25,58 @@ public class DCLab1Client
         new DCLab1Client(server, port);
     }
 
-    public DCLab1Client(String server, int port)
+    public DCLab1Client(String server, int port) throws InterruptedException
     {
         DataInputStream inputFromServer;
         DataOutputStream outputToServer;
 
-        try (Socket socket = new Socket(server, port)){
+        try {
             // create data input/output streams
-            //inputFromServer = new DataInputStream(socket.getInputStream());
-            //outputToServer = new DataOutputStream(socket.getOutputStream());
+            Socket socket = new Socket(server, port);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            OutputStream out = new BufferedOutputStream(socket.getOutputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             PrintStream pout = new PrintStream(out);
 
             // create Scanner object to read radius from the keyboard
             Scanner sc = new Scanner(System.in);
-            System.out.print(">");
 
-            while (sc.hasNext()) {
+            while (true) {
                 // get command
+                System.out.print(">");
                 String cmd = sc.nextLine();
-                
+
+                if (cmd.startsWith("exit")) {
+                    break;
+                }
                 // send radius to sever
                 System.out.println("sending command " + cmd);
                 pout.println(cmd);
                 pout.flush();
                 out.flush();
 
-                // get area from server
-                //String res = in.readLine();
-                //System.out.print("server msg is " + res);
+                System.out.println("before ready");
+                int count = 0;
+                while (!in.ready() && count < 3) { //Wait Server Answer 5 second
+                    Thread.sleep(500);
+                    count++;
+                }
+                System.out.println("after ready count is = " + count);
+                if (count >= 3) {
+                    System.out.println("Time run out");
+                } else {
+                    String res = in.readLine();
+                    System.out.println("server msg is " + res);
+                }
             }
+
+            System.out.println("Client is closing");
             pout.close();
             in.close();
             out.close();
             socket.close();
+            Thread.sleep(5000);
             System.out.println("Client closed");
+
         } catch (IOException e) {
             System.err.println(e);
         }
